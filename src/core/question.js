038ff150f2e0d4373e6500cb0da3320104583108
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-10-02 12:02:57 +0530

    Added a readline example

--- src/core/question.js
+++ src/core/question.js
@@ -1,2 +1,12 @@
 
 const readline = require("readline");
+
+const rInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rInterface.question('How are you liking this blog?', answer => {
+    console.log(`You found it, ${answer}! Thanks for the Feedback.`);
+    rInterface.close();
+});
